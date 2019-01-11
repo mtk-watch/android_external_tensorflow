89345b1ce9408b7298f3479e6a02615d45fb34eb
Nikita Iashchenko <nikitai@google.com>
2019-01-11 17:49:54 +0000

    Update includes in favor of external/libtextclassifier update
    Bug: 119788152
    Test: m droid successfully builds + atest frameworks/base/core/tests/coretests/src/android/view/textclassifier + manual testing
    Change-Id: I5a2077473460e4dc322bdd08f4e53a749487eaeb
    Merged-In: Ia565913f0ec2aa1f36110d8b50d6b778b81c674e
    (cherry picked from commit 51cd24c0466be5b280b5cf8a577180ba85735988)

--- tensorflow/contrib/lite/kernels/lsh_projection.cc
+++ tensorflow/contrib/lite/kernels/lsh_projection.cc
@@ -63,7 +63,7 @@ limitations under the License.
 #include "tensorflow/contrib/lite/c/c_api_internal.h"
 #include "tensorflow/contrib/lite/kernels/kernel_util.h"
 #include "tensorflow/contrib/lite/kernels/op_macros.h"
-#include "util/hash/farmhash.h"
+#include "utils/hash/farmhash.h"
 
 namespace tflite {
 namespace ops {
